df593a03214523931bf7cabf360ac1032a2aba87
lelio979-cyber <lelio979@gmail.com>
2026-06-18 22:42:33 -0300

    Update instalar_tudo.py

--- instalar_tudo.py
+++ instalar_tudo.py
@@ -1,1 +1,15 @@
+import subprocess
+import sys
 
+def instalar(biblioteca):
+    print(f"Instalando {biblioteca}... Por favor, aguarde.")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", biblioteca])
+    print(f"{biblioteca} instalada com sucesso!\n")
+
+# O próprio Python vai tentar instalar para você sem precisar do terminal
+try:
+    instalar("customtkinter")
+    instalar("matplotlib")
+    print("🎉 TUDO PRONTO! Agora você pode rodar o sistema de frotas.")
+except Exception as e:
+    print(f"Ocorreu um erro na instalação automática: {e}")
